983f860bf1fc8faf43ba4c8868c72fb7394d18c6
gyfkdz <root@grodvan.com>
2015-12-08 19:50:12 +0100

    asd

--- cameratest.js
+++ cameratest.js
@@ -1,14 +1,25 @@
 
 
-var Camera = require("camerapi");
-
-var cam = new Camera();
+var RaspiCam = require("raspicam");
 
 
-cam.baseDirectory('/nodeserver/piserver');
+var camera = new RaspiCam({
+	mode: "photo",
+	output: "./photo/image.jpg",
+	encoding: "jpg",
+	timeout: 0 // take the picture immediately
+});
 
-cam.prepare({"timeout" : 150, 
-		 "width" : 2592,
-		 "height" : 1944,
-		 "quality" : 85
-	   }).takePicture("mypicture.jpg");
+camera.on("started", function( err, timestamp ){
+	console.log("photo started at " + timestamp );
+});
+
+camera.on("read", function( err, timestamp, filename ){
+	console.log("photo image captured with filename: " + filename );
+});
+
+camera.on("exit", function( timestamp ){
+	console.log("photo child process has exited at " + timestamp );
+});
+
+camera.start();
