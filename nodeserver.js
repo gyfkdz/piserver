2f14ec9272957107152dcddcabfda6e291133adf
gyfkdz <root@grodvan.com>
2015-12-08 02:11:42 +0100

    first

---
+++ nodeserver.js
@@ -0,0 +1,39 @@
+//socket test
+
+
+
+var net = require('net')
+var http = require('http')
+
+//var JSON = require('JSON')
+
+
+var port = 8080;
+
+var server = http.createServer(function (request, response) {
+			
+	
+  switch(request.url) {
+
+    default:
+      response.writeHead(404, "Not found", {'Content-Type': 'text/html'});
+      response.end('<html><head><title>404 - Not found</title></head><body><h1>Not found.</h1></body></html>');
+	  
+	  
+      console.log("[404] " + request.method + " to " + request.url);
+	  
+	  
+	  
+  };
+
+
+  
+			
+
+		  
+		  
+		  
+	
+	});
+
+server.listen(port);
